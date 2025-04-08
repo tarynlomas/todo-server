dae9ee0d5f55d4d62668f4f6bee142b47db15979
tarynlomas <taryn@hive.co>
2025-04-08 15:48:28 -0700

    feat: adding cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // Main server logic
 // app -> routes -> controllers -> model 
 const express = require("express");
+const cors = require('cors');    
 const app = express();
 const todosRouter = require("./routes/todos");
 
+app.use(cors())
+// parsing the body for the POST and PATCH requests 
 app.use(express.json())
 // middlewares
 app.use("/todos", todosRouter);
